176f6f0921c7a95fae43ee42c77afbd55f1733e4
Hanson Li <lihsca@gmail.com>
2019-07-23 18:03:22 -0400

    bookmark update

--- bookmarks/pages/bookmarks.js
+++ bookmarks/pages/bookmarks.js
@@ -10,6 +10,11 @@ function render(bookmarks, el)
     return '<a href="' + bookmark.url + '">' + bookmark.desc + '</a>';
   };
 
+  var subtitle = function(str) {
+    str = str.substring(1);
+    return '<h3>' + str + '</h3>';
+  };
+
   var genLink = function(bookmark) {
     var c = bookmark.desc.charAt(0);
     if (c == ' ' || c == '#') {
@@ -22,19 +27,24 @@ function render(bookmarks, el)
 
   for (var i = 0; i < bookmarks.length; i++) {
     if (Array.isArray(bookmarks[i])) {
-      html += genLink(bookmarks[i][0]);
-      for (var j = 1; j < bookmarks[i].length; j++) {
-        html += '&nbsp;|&nbsp;';
-        html += shortLink(bookmarks[i][j]);
+      if (bookmarks[i].length > 0) {
+        html += genLink(bookmarks[i][0]);
+        for (var j = 1; j < bookmarks[i].length; j++) {
+          html += '&nbsp;|&nbsp;';
+          html += shortLink(bookmarks[i][j]);
+        }
+        html += '<br>';
       }
-      html += '<br>';
+    } else if (typeof bookmarks[i] === 'string') {
+      var text = bookmarks[i];
+      if (text.charAt(0) == '=') {
+        html += subtitle(text);
+      } else if (text.charAt(0) == '-') {
+        html += '<br>';
+      }
     } else {
-      if (bookmarks[i].url.charAt(0) == '-') {
-        html += '<br>';
-      } else {
-        html += genLink(bookmarks[i]);
-        html += '<br>';
-      }
+      html += genLink(bookmarks[i]);
+      html += '<br>';
     }
   }
 
